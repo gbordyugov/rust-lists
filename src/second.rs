5fe3d7334a6a62006e6225fe1833707fd8fa7373
Grigory Bordyugov <gregbg@gmail.com>
2021-04-12 17:04:03 +0200

    make list generic

--- src/second.rs
+++ src/second.rs
@@ -1,20 +1,20 @@
-pub struct List {
-    head: Link
+pub struct List<T> {
+    head: Link<T>
 }
 
-type Link = Option<Box<Node>>;
+type Link<T> = Option<Box<Node<T>>>;
 
-struct Node {
-    elem: i32,
-    next: Link,
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
 }
 
-impl List {
+impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
     }
 
-    pub fn push(&mut self, elem: i32) {
+    pub fn push(&mut self, elem: T) {
         let n = Node {
             elem: elem,
             next: self.head.take(),
@@ -23,7 +23,7 @@ impl List {
         self.head = Link::Some(Box::new(n))
     }
 
-    pub fn pop(&mut self) -> Option<i32> {
+    pub fn pop(&mut self) -> Option<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
@@ -31,7 +31,7 @@ impl List {
     }
 }
 
-impl Drop for List {
+impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur = self.head.take();
 
@@ -47,7 +47,7 @@ mod test {
 
     #[test]
     fn test_popping_from_empty_list() {
-        let mut list = List::new();
+        let mut list = List::<i32>::new();
 
         assert_eq!(list.pop(), None);
     }
