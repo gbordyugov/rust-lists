62b84b1a7b557a5d3c0568509ee17be5f28946d4
Grigory Bordyugov <gregbg@gmail.com>
2021-04-12 13:29:44 +0200

    re-implement drop from scratch

--- src/first.rs
+++ src/first.rs
@@ -40,13 +40,10 @@ impl List {
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, Link::Nil);
-        // `while let` == "do this thing until this pattern doesn't match"
-        while let Link::More(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, Link::Nil);
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to Link::Empty
-            // so no unbounded recursion occurs.
+        let mut cur = mem::replace(&mut self.head, Link::Nil);
+
+        while let Link::More(mut node) = cur {
+            cur = mem::replace(&mut node.next, Link::Nil)
         }
     }
 }
