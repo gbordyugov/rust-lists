5978afb0937800d7ec33acb0b8c13c8188272eb5
Grigory Bordyugov <gregbg@gmail.com>
2021-04-20 15:05:27 +0200

    reimplment iter for list from scratch

--- src/second.rs
+++ src/second.rs
@@ -82,9 +82,29 @@ impl<T> List<T> {
  */
 
 pub struct ListIterator<'a, T> {
-    next: Option<&'a Box<T>>,
+    next: Option<&'a Node<T>>,
 }
 
+impl<T> List<T> {
+    pub fn iter(&self) -> ListIterator<T> {
+        ListIterator {
+            next: self.head.as_deref()
+        }
+    }
+}
+
+impl<'a, T> Iterator for ListIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(
+            |node| {
+                self.next = node.next.as_deref();
+                &node.elem
+            }
+        )
+    }
+}
 /*
  * Make List<T> an iterator.
  */
