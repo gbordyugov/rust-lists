35b7d29661faaf12f410881ac0f19ab808105ed6
Grigory Bordyugov <gregbg@gmail.com>
2021-04-21 09:37:43 +0200

    push/pop implemented, unit tests

--- src/second.rs
+++ src/second.rs
@@ -1,8 +1,10 @@
 pub struct List<T> {
-    next: Link<T>,
+    head: Link<T>,
 }
 
+
 type Link<T> = Option<Box<Node<T>>>;
+
 
 struct Node<T>{
     elem: T,
@@ -12,18 +14,61 @@ struct Node<T>{
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        unimplemented!()
+        List { head: None }
     }
 
-    pub fn push(&mut self) {
-        unimplemented!()
+    pub fn push(&mut self, t: T) {
+        let new_node = Node {
+            elem: t,
+            next: self.head.take(),
+        };
+
+        self.head = Some(Box::new(new_node));
     }
 
-    pub fn pop(&mut self, elem: T) -> Self {
-        unimplemented!()
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(
+            |node| {
+                self.head = node.next;
+                node.elem
+            }
+        )
     }
 
     pub fn peek(&self) -> &T {
         unimplemented!()
+    }
+}
+
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        List::new()
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn popping_from_empty_list() {
+        let mut list = List::<i32>::new();
+
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn popping_from_non_empty_list() {
+        let mut list = List::new();
+        for i in 1..4 {
+            list.push(i)
+        }
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
     }
 }
