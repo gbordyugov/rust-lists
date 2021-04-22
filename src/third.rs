8b3d7b1cf3034081326f6227555c3000b5fd72c9
Grigory Bordyugov <gregbg@gmail.com>
2021-04-22 11:11:22 +0200

    implementation for the persisent stack

--- src/third.rs
+++ src/third.rs
@@ -43,4 +43,26 @@ impl<T> List<T> {
             head: Some(Rc::new(node))
         }
     }
+
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            // Option.and_then() is just a flatMap().
+            head: self.head.as_ref().and_then(
+                |node| {
+                    node.next.clone()
+                }
+            )
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+impl<T> Default for List<T> {
+    fn default() -> Self {
+        Self::new()
+    }
 }
