6d835bc5f14326cac930c5901e369c3528969082
Grigory Bordyugov <gregbg@gmail.com>
2021-04-23 21:00:37 +0200

    implement cons/head/tail plus unit tests

--- src/third.rs
+++ src/third.rs
@@ -20,16 +20,27 @@ impl<T> List<T> {
         List { head: None }
     }
 
-    pub fn cons(&self, elem: T) -> Self {
-        unimplemented!()
+    pub fn cons(&self, t: T) -> Self {
+        let node = Node {
+            elem: t,
+            next: self.head.clone()
+        };
+
+        List { head: Some(Rc::new(node)) }
     }
 
     pub fn head(&self) -> Option<&T> {
-        unimplemented!()
+        self.head.as_ref().map(
+            |node| &node.elem
+        )
     }
 
     pub fn tail(&self) -> List<T> {
-        unimplemented!()
+        List {
+            head: self.head.as_ref().and_then(
+                |node| node.next.clone()
+            )
+        }
     }
 }
 
@@ -40,9 +51,34 @@ impl<T> Default for List<T> {
     }
 }
 
-
+/*
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         unimplemented!()
+    }
+}
+*/
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+
+        assert_eq!(list.head(), None);
+
+        let list = list.cons(1).cons(2).cons(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
     }
 }
